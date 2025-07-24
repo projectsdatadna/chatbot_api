362dfac13bfdd6b99ebf009fde35afd2ef7ff22f
projectsdatadna <projects@datadna.in>
2025-07-24 18:47:38 +0530

    cors policy changes

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const app = express()
 
 const allowedOrigin = process.env.ALLOWED_ORIGIN || "*";
 
-app.use(cors({ origin: allowedOrigin }));
+app.use(cors({
+  origin: [allowedOrigin],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
 app.use(express.json())
 app.use(bodyParser.json());
